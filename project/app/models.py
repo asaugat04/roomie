87cc61d87f7a81b66f4b279386bc9a51e86b3d61
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-10 22:58:04 +0545

    extended user's info

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Profile(models.Model):
+    food_preference_choices = [
+        ('Veg', 'Veg'),
+        ('Non-Veg', 'Non-Veg'),
+    ]
+    gender_choices = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Others', 'Others'),
+    ]
+    user_type_choices = [
+        ('Single','Single'),
+        ('Couple','Couple'),
+        ('Family','Family'),
+    ]
+    user_type_2_choices = [
+        ('Student','Student'),
+        ('Employee','Employee'),
+        ('Others','Others'),
+    ]
 
-# Create your models here.
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    temp_address = models.CharField(max_length=100)
+    perm_address = models.CharField(max_length=100)
+    phone_no = models.IntegerField()
+    image = models.ImageField(upload_to='profile_image', blank=True, null=True)
+    food_preference = models.CharField(max_length=10, choices=food_preference_choices)
+    gender = models.CharField(max_length=10, choices=gender_choices)
+    user_type = models.CharField(max_length=10, choices=user_type_choices)
+    user_type_2 = models.CharField(max_length=10, choices=user_type_2_choices)
+    interests = models.TextField()
+
+
+
+
+    def get_full_name(self):
+        return self.user.first_name + " " + self.user.last_name
+    def __str__(self):
+        return self.user.username
