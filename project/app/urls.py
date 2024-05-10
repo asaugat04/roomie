0779d301b29d11b0883a4e4d8d5caa54c1b861d0
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-10 23:40:04 +0545

    urls created for login and signup

--- project/app/urls.py
+++ project/app/urls.py
@@ -6,4 +6,6 @@ app_name = "app"
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('accounts/login/', views.login, name="login"),
+    path('accounts/signup/', views.signup, name="signup"),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home(request):
-    return render(request,"app/home.html")
+    return HttpResponse("home")
+    # return render(request,"app/signup.html")
+
+
+
+def signup(request):
+    return render(request,"app/signup.html")
+
+
+def login(request):
+    return render(request,"app/login.html")
